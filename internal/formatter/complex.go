1c6ada41822db95c1e52143cd5df14f2b6734b12
Viktor <viktor.pakhuchyi@gmail.com>
2023-11-10 16:53:45 +0100

    feat: add complex formatter tests

--- internal/formatter/complex.go
+++ internal/formatter/complex.go
@@ -11,6 +11,10 @@ import (
 // The value is formatted with up to 7 decimal places for both parts of complex64 and
 // up to 15 decimal places for both parts of complex128.
 func (f *Formatter) Complex(v models.Value) string {
+	if v.Kind != reflect.Complex64 && v.Kind != reflect.Complex128 {
+		panic("provided value is not a complex")
+	}
+
 	if v.Kind == reflect.Complex64 {
 		return fmt.Sprintf(`%.7g`, v.Value)
 	}
---
+++ internal/formatter/complex_test.go
@@ -0,0 +1,42 @@
+package formatter
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/vpakhuchyi/censor/internal/models"
+)
+
+func TestFormatter_Complex(t *testing.T) {
+	f := Formatter{
+		MaskValue:         DefaultMaskValue,
+		DisplayStructName: false,
+		DisplayMapType:    false,
+	}
+
+	t.Run("complex64", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			v := models.Value{Value: complex(float32(3.11111111111111), float32(-2345.2352325353)), Kind: reflect.Complex64}
+			got := f.Complex(v)
+			exp := "(3.111111-2345.235i)"
+			require.Equal(t, exp, got)
+		})
+	})
+
+	t.Run("complex128", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			v := models.Value{Value: complex(float64(343.11111111111111), float64(-2345.9992352325353)), Kind: reflect.Complex128}
+			got := f.Complex(v)
+			exp := "(343.111111111111-2345.99923523254i)"
+			require.Equal(t, exp, got)
+		})
+	})
+
+	t.Run("non_complex_value", func(t *testing.T) {
+		require.PanicsWithValue(t, "provided value is not a complex", func() {
+			f.Complex(models.Value{Value: 44, Kind: reflect.Int})
+		})
+	})
+}
