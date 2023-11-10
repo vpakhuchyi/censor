dacdd5b21603975c87360412ad36b6f953257fa5
Viktor <viktor.pakhuchyi@gmail.com>
2023-11-10 19:00:00 +0100

    feat: add float formatter tests

--- internal/formatter/float.go
+++ internal/formatter/float.go
@@ -8,8 +8,12 @@ import (
 )
 
 // Float formats a value as a float.
-// The value is formatted with up to 7 decimal places for float32 and up to 15 decimal places for float64.
+// The value is formatted with up to 7 g places for float32 and up to 15 decimal places for float64.
 func (f *Formatter) Float(v models.Value) string {
+	if v.Kind != reflect.Float32 && v.Kind != reflect.Float64 {
+		panic("provided value is not a float")
+	}
+
 	if v.Kind == reflect.Float32 {
 		return fmt.Sprintf(`%.7g`, v.Value)
 	}
---
+++ internal/formatter/float_test.go
@@ -0,0 +1,42 @@
+package formatter
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/vpakhuchyi/censor/internal/models"
+)
+
+func TestFormatter_Float(t *testing.T) {
+	f := Formatter{
+		MaskValue:         DefaultMaskValue,
+		DisplayStructName: false,
+		DisplayMapType:    false,
+	}
+
+	t.Run("float32", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			v := models.Value{Value: float32(3.11111111111111), Kind: reflect.Float32}
+			got := f.Float(v)
+			exp := "3.111111"
+			require.Equal(t, exp, got)
+		})
+	})
+
+	t.Run("float64", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			v := models.Value{Value: float64(3.11111111111111), Kind: reflect.Float64}
+			got := f.Float(v)
+			exp := "3.11111111111111"
+			require.Equal(t, exp, got)
+		})
+	})
+
+	t.Run("non_float_value", func(t *testing.T) {
+		require.PanicsWithValue(t, "provided value is not a float", func() {
+			f.Float(models.Value{Value: 44, Kind: reflect.Int})
+		})
+	})
+}
