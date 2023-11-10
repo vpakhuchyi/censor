d17ddccba2e364a116b8d8436f1c6894239ec28d
Viktor <viktor.pakhuchyi@gmail.com>
2023-11-10 19:07:17 +0100

    feat: add string formatter tests

--- internal/formatter/string.go
+++ internal/formatter/string.go
@@ -2,11 +2,16 @@ package formatter
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/vpakhuchyi/censor/internal/models"
 )
 
 // String formats a value as a string.
 func (f *Formatter) String(v models.Value) string {
+	if v.Kind != reflect.String {
+		panic("provided value is not a string")
+	}
+
 	return fmt.Sprintf(`%s`, v.Value)
 }
---
+++ internal/formatter/string_test.go
@@ -0,0 +1,33 @@
+package formatter
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/vpakhuchyi/censor/internal/models"
+)
+
+func TestFormatter_String(t *testing.T) {
+	f := Formatter{
+		MaskValue:         DefaultMaskValue,
+		DisplayStructName: false,
+		DisplayMapType:    false,
+	}
+
+	t.Run("string", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			v := models.Value{Value: "hello", Kind: reflect.String}
+			got := f.String(v)
+			exp := "hello"
+			require.Equal(t, exp, got)
+		})
+	})
+
+	t.Run("non_string_value", func(t *testing.T) {
+		require.PanicsWithValue(t, "provided value is not a string", func() {
+			f.String(models.Value{Value: 44, Kind: reflect.Int})
+		})
+	})
+}
