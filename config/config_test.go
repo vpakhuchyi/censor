91b39935ce8bbe4f115d8a83322ed374126b6553
Viktor Pakhuchyi <viktor.pakhuchyi@gmail.com>
2024-01-04 14:33:59 +0200

    feat: [CN-26] add a possibility to use a YAML config file (#12)

--- config/config_test.go
+++ config/config_test.go
@@ -45,3 +45,73 @@ func TestConfig_GetFormatterConfig(t *testing.T) {
 
 	require.EqualValues(t, exp, got)
 }
+
+func TestFromFile(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := map[string]struct {
+		args    args
+		want    Config
+		wantErr bool
+	}{
+		"successful": {
+			args: args{
+				path: "./testdata/cfg.yml",
+			},
+			want: Config{
+				Parser: Parser{
+					UseJSONTagName: true,
+				},
+				Formatter: Formatter{
+					MaskValue:            "[CENSORED]",
+					DisplayPointerSymbol: true,
+					DisplayStructName:    true,
+					DisplayMapType:       true,
+					ExcludePatterns:      []string{`\d`},
+				},
+			},
+			wantErr: false,
+		},
+		"empty_cfg_file_path": {
+			args: args{
+				path: "",
+			},
+			want:    Config{},
+			wantErr: true,
+		},
+		"empty_cfg_file_content": {
+			args: args{
+				path: "./testdata/empty.yml",
+			},
+			want:    Config{},
+			wantErr: false,
+		},
+		"invalid_file_path": {
+			args: args{
+				path: "./testdata/non-existing.yml",
+			},
+			want:    Config{},
+			wantErr: true,
+		},
+		"invalid_file_content": {
+			args: args{
+				path: "./testdata/invalid_cfg.yml",
+			},
+			want:    Config{},
+			wantErr: true,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := FromFile(tt.args.path)
+			require.Equal(t, tt.want, got)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
