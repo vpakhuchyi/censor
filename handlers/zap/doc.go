5af474c6a8ea7cdc8a75dc31dbbb9e374c38fe19
Viktor <viktor.pakhuchyi@gmail.com>
2024-02-08 23:04:31 +0100

    fix: [CN-40] update zap handler doc

--- handlers/zap/doc.go
+++ handlers/zap/doc.go
@@ -2,11 +2,15 @@
 Package zaphandler provides a configurable logging handler for github.com/uber-go/zap library, allowing users
 to apply censoring to log entries and fields, overriding the original values before passing them to the core.
 
-Please pay attention to log.Infof(template, ...args).
-Because it uses fmt.Sprintf, it can't be censored using all the Censor instrument.
-The only thing that can be used is a comparison of all the strings against regex patterns.
+Due to the diversity of the zap library usage, please pay attention to the way you use it with the censor handler.
 
-Example struct with censor tags:
+Example of Censor handler initialization:
+
+	import (
+		censorlog "github.com/vpakhuchyi/censor/handlers/zap"
+		"go.uber.org/zap"
+		"go.uber.org/zap/zapcore"
+	)
 
 	type User struct {
 		Name  string `censor:"display"`
@@ -18,47 +22,63 @@ Example struct with censor tags:
 		Email: "example@gmail.com",
 	}
 
-Usage with default options:
+	o := zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return censorlog.NewHandler(core)
+	})
 
-	handler := NewJSONHandler()
-	log := slog.New(handler)
-	log.Info("user", slog.Any("payload", u))
-
-Output:
-{"time":"2023-12-28T20:15:45.893115+01:00","level":"INFO","msg":"user","payload":"{Name: John Doe, Email: [CENSORED]}"}
-
-Usage with custom options:
-
-	censorInst := censor.New()
-	opts := []Option{
-		WithOut(os.Stdout),
-		WithCensor(censorInst),
-		WithAddSource(),
-		WithReplaceAttr(func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == "msg" {
-				return slog.Any("msg", "replaced msg")
-			}
-
-			return a
-		}),
+	l, err := zap.NewProduction(o)
+	if err != nil {
+		// handle error
 	}
 
-	handler := NewJSONHandler(opts...)
-	log := slog.New(handler)
-	log.Info("user", slog.Any("payload", u))
+	l.Info("user", zap.Any("payload", u))
 
 Output:
+{"level":"info",...,"msg":"user","payload":"{Name: John Doe, Email: [CENSORED]}"}
 
-		{
-	    	"time": "2023-12-28T20:22:44.35868+01:00",
-	    	"level": "INFO",
-	    	"source": {
-	        	"function": "github.com/vpakhuchyi/censor/handlers/slog.TestFunc",
-	        	"file": "/Users/volodymyr/Files/source/censor/handlers/slog/handler_test.go",
-	        	"line": "156"
-	    	},
-	    	"msg": "replaced msg",
-	    	"payload": "{Name: John Doe, Email: [CENSORED]}"
-		}
+Describing the logger usage we can operate with a few keywords: "msg", "key" and "value".
+For example, in a call to l.Info("payload", zap.Any("addresses", []string{"address1", "address2"})):
+  - "payload" is a "msg"
+  - "addresses" is a "key"
+  - []string{"address1", "address2"} is a "value"
+
+By default, Censor processes only "value" to minimize the overhead. The "msg" and "key" rarely contain sensitive data.
+However, there are predefined configuration options that can be passed to NewHandler() function to customize the
+censor handler behavior.
+
+The following options are available:
+1. WithCensor(censor *censor.Processor) - sets the censor processor instance for the zap handler.
+If not provided, a default censor processor is used.
+2. WithMessagesFormat() - enables the censoring of a log "msg" values if such are present.
+3. WithKeysFormat() - enables the censoring of log "key" values.
+
+After the handler is initialized, it can be used as a regular zap logger. Because the censor handler is a wrapper around
+github.com/uber-go/zap library logic, it may not be compatible with all the possible ways of the logger usage.
+
+That's why it's recommended to use it with the following constructions:
+
+  - l.Info(msg string, fields ...zap.Field)
+
+  - l.With(fields ...zap.Field).Info(msg string, fields ...zap.Field)
+
+    In both cases, Info could be replaced with Debug, Warn, Error, Panic, Fatal.
+
+With sugared logger, the following constructions are supported:
+
+  - l.Info(args ...interface{})
+
+  - l.Infof(template string, args ...interface{})
+
+  - l.Infow(msg string, keysAndValues ...interface{})
+
+  - l.Infoln(args ...interface{})
+
+  - l.With(args ...interface{}).Info(args ...interface{})
+
+    In all cases, Info could be replaced with Debug, Warn, Error, Panic, Fatal.
+
+Methods ending in "f", "ln" and log.Print-style (l.Info) in a sugared logger can't use all the
+censor handler features. Due to the nature of the zap sugared logger, Censor accepts formatted strings and has no
+possibility to use its parsing. However, a features like regexp matching are still available.
 */
 package zaphandler
