2c1d3409fb91c9770672896ab426eb7d7cff57c5
Viktor <viktor.pakhuchyi@gmail.com>
2025-01-21 22:32:37 +0100

    feat: [NA] improve regex patterns handling

--- internal/encoder/regexp.go
+++ internal/encoder/regexp.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 )
 
-// compileExcludePatterns returns compiled regexp patterns.
+// compileExcludePatterns returns compiled regexp patterns joined by "|".
 // Note: this method may panic if any regexp pattern is invalid.
 func compileRegexpPatterns(patterns []string) *regexp.Regexp {
-	if len(patterns) > 0 {
-		return regexp.MustCompile(strings.Join(patterns, "|"))
+	if len(patterns) == 0 {
+		return nil
 	}
 
-	return nil
+	return regexp.MustCompile(strings.Join(patterns, "|"))
 }
